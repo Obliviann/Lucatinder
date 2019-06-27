aa3c2f09075334effa99597a155e509e221a38bc
Daniel Galan <danigane99@gmail.com>
2019-06-27 14:31:01 +0200

    Estructura proyecto

---
+++ src/main/java/com/example/demo/datos/UsuarioDao.java
@@ -0,0 +1,5 @@
+package com.example.demo.datos;
+
+public interface UsuarioDao {
+
+}
---
+++ src/main/java/com/example/demo/servicios/GenericRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.servicios;
+
+import java.io.Serializable;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+public class GenericRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements GenericRepository<T, ID> {
+
+	private final EntityManager entityManager;
+
+	public GenericRepositoryImpl(Class<T> domainClass, EntityManager entityManager) {
+		super(domainClass, entityManager);
+		this.entityManager = entityManager;
+	}
+
+}

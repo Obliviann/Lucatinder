a98983550082a07d2f4e5eeae7c09648893ef938
Daniel Galan <danigane99@gmail.com>
2019-07-02 09:17:49 +0200

    like

--- src/main/java/com/example/demo/controller/ControllerTinder.java
+++ src/main/java/com/example/demo/controller/ControllerTinder.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.Usuario;
 import com.example.demo.servicios.UsuarioService;
@@ -16,46 +17,53 @@ import com.example.demo.servicios.UsuarioService;
 
 @Controller
 public class ControllerTinder {
-
-	@Autowired
-	private UsuarioService usuarioService;
-	
-	private static final Logger logger = LoggerFactory.getLogger(Controller.class);
-	
-	@GetMapping("/")
-	public String inicio(ModelMap model) {
-		logger.info(" --- En INICIO ");
-		model.addAttribute("user", new Usuario());
-		return "inicio";
-	}
-	
-	@RequestMapping("/listado")
-	public String listado(ModelMap model) {
-		logger.info("En el listado");
-		model.addAttribute("listado", usuarioService.generateTen());
-		return "bienvenida";
-	}
-	
-	@PostMapping("/save")
-	public String saveUser(@ModelAttribute Usuario user) {
-		logger.info("RegistroUsuario");
-		usuarioService.create(user);
-		return "redirect:/listado";
-	}
-	
-	@PostMapping("/login")
-	public String loginUser(@ModelAttribute int id, ModelMap model) {
-		if(usuarioService.findById(id)!=null) {
-			model.addAttribute("user", usuarioService.findById(id));
-			return "redirect:/listado";
-		}else{
-			return "redirect:/inicio";
-		}
-	}
-	
-	
-	
-	
-	
-	
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    private static final Logger logger = LoggerFactory.getLogger(ControllerTinder.class);
+    
+    @GetMapping("/")
+    public String inicio(ModelMap model) {
+        logger.info(" --- En INICIO ");
+        model.addAttribute("user", new Usuario());
+        return "inicio";
+    }
+    
+    @RequestMapping("/listado")
+    public String listado(@ModelAttribute("user") Usuario user ,ModelMap model) {
+        logger.info("En el listado");
+        model.addAttribute("user", user);
+        model.addAttribute("listado", usuarioService.generateTen());
+        return "bienvenida";
+    }
+    
+    @PostMapping("/save")
+    public String saveUser(@ModelAttribute Usuario user) {
+        logger.info("RegistroUsuario");
+        usuarioService.create(user);
+        return "redirect:/listado";
+    }
+    
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute ("user") Usuario user, ModelMap model) {
+        if(usuarioService.findById(user.getIdusuario())!=null) {
+            model.addAttribute("user", usuarioService.findById(user.getIdusuario()));
+            model.addAttribute("listado", usuarioService.generateTen());
+            return "bienvenida";
+        }else{
+            return "redirect:/";
+        }
+    }
+    
+    /* HECHO EL 01/07/2019 */
+    @GetMapping("/like")
+    public String like(@RequestParam("id1") int id1, @RequestParam("id2") int id2, ModelMap model) {
+        logger.info("----- Ejecutando query en el Controller -----");
+        logger.info("id1 que recibe el REST: "+id1+" -- | -- id2 que recibe el REST: "+id2);
+        usuarioService.like(id1, id2);
+        model.addAttribute("user", usuarioService.findById(id1));
+        model.addAttribute("listado", usuarioService.generateTen());
+        return "bienvenida";
+    } 
+    
 }
--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,50 +22,46 @@ import com.example.demo.servicios.UsuarioService;
 @RequestMapping({ "/usuarios" })
 public class UsuarioController {
 
+	private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
-	
+
 	@PostMapping
 	public Usuario create(@RequestBody Usuario usuario) {
 		return usuarioService.create(usuario);
 	}
-	
+
 	@GetMapping(path = { "/{idusuario}" })
 	public Usuario findOne(@PathVariable("idusuario") int id) {
 		return usuarioService.findById(id);
 	}
-	
+
 	@PutMapping()
 	public Usuario update(@PathVariable("idusuario") int id, @RequestBody Usuario usuario) {
 		usuario.setIdusuario(id);
 		return usuarioService.update(usuario);
-		
+
 	}
-	
-	
-	/*@DeleteMapping(path = { "/{idusuario}" })
-	public Usuario delete(@PathVariable("idusuario") int id) {
-		return usuarioService.deleteById(id);
-	}*/
-	
-	
+
 	@DeleteMapping(path = { "/{idusuario}" })
 	public Usuario delete(@PathVariable("idusuario") int id) {
 		return usuarioService.deleteById(id);
 	}
 
-	/*
-	@GetMapping
-	public List<Usuario> findAll() {
-		return usuarioService.findAll();
-	}*/
-	
-	
-	//Rest para añadir generar los 10 usuarios
+	// Rest para añadir generar los 10 usuarios
 	@GetMapping(path = { "/ten" })
 	public List<Usuario> generateTen() {
 		return usuarioService.generateTen();
 	}
-	
+
+	/* HECHO EL 01/07/2019 */
+	@PostMapping("/like")
+	public void like(@RequestBody int id1, @RequestBody int id2) {
+		logger.info(
+				"----- Ejecutando query en el servicio REST. Ruta del paquete: controller.UsuarioController.java -----");
+		logger.info("id1 que recibe el REST: " + id1 + " -- | -- id2 que recibe el REST: " + id2);
+		usuarioService.like(id1, id2);
+
+	}
 }

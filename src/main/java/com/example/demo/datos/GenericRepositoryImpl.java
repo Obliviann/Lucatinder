690399018bf7bd0430bbfd35de6c0bedc10b6f94
Marian Drozd <mariandrozd98@gmail.com>
2019-07-02 09:03:11 +0200

    .

--- src/main/java/com/example/demo/datos/GenericRepositoryImpl.java
+++ src/main/java/com/example/demo/datos/GenericRepositoryImpl.java
@@ -1,19 +1,11 @@
 package com.example.demo.datos;
 
 import java.io.Serializable;
-
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.servicios.UsuarioServiceImpl;
 
 
 public class GenericRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements GenericRepository<T, ID> {
